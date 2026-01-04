1394e3a3c35f755aeec69102c58f738c11f841c0
sshiv <sshivprasad405@gmail.com>
2026-01-04 23:19:26 +0530

    Add debug logging and output file

--- 1. Introduction/1. The Problem.js
+++ 1. Introduction/1. The Problem.js
@@ -3,6 +3,7 @@ const studentsDatabase = ["jordan", "erick", "john", "michel"];
 
 // Algorithm for finding a specific user
 const findStudent = (allStudents, studentName) => {
+  console.log("Searching for " + studentName);
   for (let i = 0; i < allStudents.length; i++) {
     if (allStudents[i] === studentName) {
       console.log(`Found ${studentName}`);
@@ -11,3 +12,5 @@ const findStudent = (allStudents, studentName) => {
 };
 
 findStudent(studentsDatabase, "erick");
+findStudent(studentsDatabase, "john");
+findStudent(studentsDatabase, "michel");
